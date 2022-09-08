e326e0a336ef5f8483efec208d7e2f74d58a41ee
zoutao98 <ezoutao@foxmail.com>
2022-09-08 22:47:07 +0800

    下拉列表选择串口

--- app_instance.py
+++ app_instance.py
@@ -12,12 +12,14 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
-from PySide2.QtCore import Signal
+from PySide2.QtCore import Signal, QObject
 from PySide2.QtWidgets import QMainWindow, QApplication
 from PySide2.QtGui import QTextCursor
 
 from main_view import Ui_MainWindow
 from app_logging import getLogger
+
+import serial.tools.list_ports
 
 log = getLogger("view")
 
@@ -28,8 +30,9 @@ class AppWindow(QMainWindow, Ui_MainWindow):
     show_txt_on_edit_signal = Signal(str)
 
     def __init__(self):
-        super().__init__()
+        super(AppWindow, self).__init__()
         self.setupUi(self)
+        self.comboBox.installEventFilter(self)
 
         self.pushButton.clicked.connect(self.handleClick)
 
@@ -39,8 +42,23 @@ class AppWindow(QMainWindow, Ui_MainWindow):
         self.show_txt_on_edit_signal.connect(self.showTxtOnTextEdit)
 
 
-        self.comboBox.addItem("hello")
-        self.comboBox.addItem("hellll")
+        self.comboBox.addItem("serial port")
+    
+    def eventFilter(self, watched, event):
+        if watched == self.comboBox and event.type() == event.MouseButtonPress or event.type() == event.MouseButtonDblClick:
+            log.info(f"comboBox event {event.type()}")
+            self.comboBox.clear()
+            __port_list = serial.tools.list_ports.comports()
+            if len(__port_list) <= 0:
+                log.error("No serial port found")
+            else:
+                for __port in __port_list:
+                    log.info(__port.description)
+                    log.info(__port.name)
+                    self.comboBox.addItem(__port.description)
+        return super(AppWindow, self).eventFilter(watched, event)
+        # return False
+    
     def handleClick(self):
         log.info("click")
         self.btn_click_signal.emit()
@@ -49,17 +67,19 @@ class AppWindow(QMainWindow, Ui_MainWindow):
         log.debug("ComboBox Clicked")
 
     def showMessageOnStatusBar(self, message):
-        self.statusbar.showMessage(message)
+        self.statusbar.showMessage(message, 500)
     
 
     def showTxtOnTextEdit(self, txt):
         # tc = self.textEdit.textCursor()
         # tc.movePosition(QTextCursor.End)
         # tc.insertText(txt)
-
         self.textEdit.moveCursor(QTextCursor.End)
         self.textEdit.insertPlainText(txt)
         self.textEdit.moveCursor(QTextCursor.End)
+    
+
+    
         
 
 
