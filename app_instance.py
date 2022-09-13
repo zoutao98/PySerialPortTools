da978c33f0c57691d102048921a12e00336f63c1
zoutao98 <ezoutao@foxmail.com>
2022-09-13 09:07:57 +0800

    0913

--- app_instance.py
+++ app_instance.py
@@ -12,7 +12,7 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
-from PySide2.QtCore import Signal, QObject
+from PySide2.QtCore import Signal, QObject, QThread
 from PySide2.QtWidgets import QMainWindow, QApplication
 from PySide2.QtGui import QTextCursor
 
@@ -21,55 +21,67 @@ from app_logging import getLogger
 
 import serial.tools.list_ports
 
-log = getLogger("view")
+_log = getLogger("view")
+
+port_list = serial.tools.list_ports.comports()
+
+class ComboBoxEventFilter(QObject):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def setJudgeComboBox(self, comboBox):
+        self.comboBox = comboBox
+
+    def eventFilter(self, watched, event):
+        if watched == self.comboBox and event.type() == event.MouseButtonPress or event.type() == event.MouseButtonDblClick:
+            _log.debug(f"comboBox event: {event.type()}")
+            self.comboBox.clear()
+            self.comboBox.addItem("please select serial port")
+            global port_list 
+            port_list = serial.tools.list_ports.comports()
+            _log.debug(port_list)
+            if len(port_list) <= 0:
+                _log.error("No serial port found")
+            else:
+                for __port in port_list:
+                    _log.debug(__port.description)
+                    _log.debug(__port.name)
+                    self.comboBox.addItem(__port.description)
+        return super().eventFilter(watched, event)
+
+comboBoxEventFilter = ComboBoxEventFilter()
 
 class AppWindow(QMainWindow, Ui_MainWindow):
 
     btn_click_signal = Signal()
+    btn_change_signal = Signal()
     show_msg_on_statusbar_signal = Signal(str)
     show_txt_on_edit_signal = Signal(str)
 
     def __init__(self):
         super(AppWindow, self).__init__()
         self.setupUi(self)
-        self.comboBox.installEventFilter(self)
+        comboBoxEventFilter.setJudgeComboBox(self.comboBox)
+        self.comboBox.installEventFilter(comboBoxEventFilter)
 
         self.pushButton.clicked.connect(self.handleClick)
+        self.btn_change_signal.connect(self.handleBtnChange)
 
         self.show_msg_on_statusbar_signal.connect(self.showMessageOnStatusBar)
 
         self.textEdit.setReadOnly(True)
         self.show_txt_on_edit_signal.connect(self.showTxtOnTextEdit)
 
+        self.comboBox.addItem("please select serial port")
 
-        self.comboBox.addItem("serial port")
-    
-    def eventFilter(self, watched, event):
-        if watched == self.comboBox and event.type() == event.MouseButtonPress or event.type() == event.MouseButtonDblClick:
-            log.info(f"comboBox event {event.type()}")
-            self.comboBox.clear()
-            __port_list = serial.tools.list_ports.comports()
-            if len(__port_list) <= 0:
-                log.error("No serial port found")
-            else:
-                for __port in __port_list:
-                    log.info(__port.description)
-                    log.info(__port.name)
-                    self.comboBox.addItem(__port.description)
-        return super(AppWindow, self).eventFilter(watched, event)
-        # return False
-    
     def handleClick(self):
-        log.info("click")
         self.btn_click_signal.emit()
+    def handleBtnChange(self):
+        self.pushButton.setText("关闭串口")
     
-    def handleComboBoxClick(self):
-        log.debug("ComboBox Clicked")
-
     def showMessageOnStatusBar(self, message):
         self.statusbar.showMessage(message, 500)
     
-
     def showTxtOnTextEdit(self, txt):
         # tc = self.textEdit.textCursor()
         # tc.movePosition(QTextCursor.End)
@@ -77,11 +89,6 @@ class AppWindow(QMainWindow, Ui_MainWindow):
         self.textEdit.moveCursor(QTextCursor.End)
         self.textEdit.insertPlainText(txt)
         self.textEdit.moveCursor(QTextCursor.End)
-    
-
-    
-        
-
 
 application = QApplication()
 appWindow = AppWindow()
--- serial_port_thread.py
+++ serial_port_thread.py
@@ -13,13 +13,22 @@
 #    limitations under the License.
 
 import asyncio
+import serial
 from threading import Thread
 import serial_asyncio
 
 from app_instance import appWindow
 from app_logging import getLogger
 
-log = getLogger("serial")
+from PySide2 import QtSerialPort
+
+_log = getLogger("serial")
+
+__port_name = None
+
+class SerailDevice():
+    def __init__(self) -> None:
+        self.serial = serial.Serial()
 
 
 class SerialPortProtocol(asyncio.Protocol):
@@ -28,19 +37,33 @@ class SerialPortProtocol(asyncio.Protocol):
         self.transport = transport
         
     def connection_lost(self, exc):
-        log.warn('port closed')
+        _log.warn('port closed')
         asyncio.get_event_loop().stop()
     def data_received(self, data):
-        log.info(data)
         appWindow.show_txt_on_edit_signal.emit(data.decode('utf-8'))
 
-
 def serial_thread_loop_task(loop):
-    coro = serial_asyncio.create_serial_connection(loop, SerialPortProtocol, "COM5", baudrate=115200)
+    global __port_name
+    coro = serial_asyncio.create_serial_connection(loop, SerialPortProtocol, __port_name, baudrate=115200)
     loop.run_until_complete(coro)
     loop.run_forever()
 
+
+def serial_thread_func():
+    _log.debug("thread run")
+
 def start_serial_port():
-    my_serial_thread = Thread(target=serial_thread_loop_task, args=(asyncio.new_event_loop(),))
-    my_serial_thread.daemon = True
-    my_serial_thread.start()
+    # my_serial_thread = Thread(target=serial_thread_loop_task, args=(asyncio.new_event_loop(),))
+    # my_serial_thread.daemon = True
+    # my_serial_thread.start()
+    import app_instance
+    if (len(app_instance.port_list) > 0) and (appWindow.comboBox.currentIndex() > 0):
+        global __port_name
+        __port_name = app_instance.port_list[appWindow.comboBox.currentIndex() - 1].name
+        _log.debug(__port_name)
+        appWindow.btn_change_signal.emit()
+        # _serail_thread = Thread(name="SerialThread", target=serial_thread_func)
+        # _log.debug(_serail_thread.is_alive())
+        # _serail_thread.start()
+
+appWindow.btn_click_signal.connect(start_serial_port)
